e49326ebd422fe6f903ef7634c57a0a64e6c1da4
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-08-22 18:12:03 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(EasyLocalization(supportedLocales: const [Locale('en'),Locale('hu'),Locale('de')], path: 'assets/i18n', fallbackLocale: const Locale('en'), child: const AiNavApp()));
+}
