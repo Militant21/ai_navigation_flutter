9028c48374feebd98e454f286969d1cd99b9f064
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-08-28 16:48:37 +0200

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 
-import 'background_services.dart';
-import 'app.dart'; // ha van külön gyökér widgeted, maradhat
+import 'app.dart';
+import 'background_services.dart'; // <- ezekből hívunk
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
 
-  // Értesítések + háttérszerviz indítás
+  // Értesítések + háttérszolgáltatás inicializálása
   await initLocalNotifications();
   await startBackgroundService();
 
@@ -17,7 +17,7 @@ Future<void> main() async {
       supportedLocales: const [Locale('en'), Locale('hu'), Locale('de')],
       path: 'assets/i18n',
       fallbackLocale: const Locale('en'),
-      child: const AiNavApp(), // vagy a saját root widget-ed
+      child: const AiNavApp(),
     ),
   );
 }
