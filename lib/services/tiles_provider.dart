cf4ea07fd8ec5a119acc4e9d5c81cdbd711347ec
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-08-28 21:02:52 +0200

    tiles_provider.dart

--- lib/services/tiles_provider.dart
+++ lib/services/tiles_provider.dart
@@ -3,21 +3,20 @@ import 'package:vector_map_tiles/vector_map_tiles.dart';
 import 'package:vector_map_tiles_pmtiles/vector_map_tiles_pmtiles.dart';
 import 'package:vector_tile_renderer/vector_tile_renderer.dart' as vtr;
 
-/// Alap (világos) téma a 5.x API-hoz.
+/// Gyors téma helper – a 5.x rendererben van `createDayTheme()`.
 vtr.Theme defaultMapTheme() => vtr.createDayTheme();
 
 /// PMTiles fájlból VectorTileLayer készítése.
-/// [pmtileFile] a .pmtiles fájl.
-/// [theme] opcionális; ha nincs megadva, defaultMapTheme() lép életbe.
-Future<VectorTileLayer> pmtilesLayer(File pmtileFile, [vtr.Theme? theme]) async {
-  if (!pmtileFile.existsSync()) {
-    throw StateError('PMTiles file nem található: ${pmtileFile.path}');
+Future<VectorTileLayer> pmtilesLayer(
+  File pmtilesFile, {
+  vtr.Theme? theme,
+}) async {
+  if (!await pmtilesFile.exists()) {
+    throw StateError('PMTiles file not found: ${pmtilesFile.path}');
   }
 
-  // Helyes provider példányosítás (konstruktor, nem fromFile)
-  final prov = await PmTilesVectorTileProvider.fromSource(pmtileFile.path);
+  final prov = await PmTilesVectorTileProvider.fromSource(pmtilesFile.path);
 
-  // A Protomaps témák a 'protomaps' forrásnévre számítanak.
   return VectorTileLayer(
     theme: theme ?? defaultMapTheme(),
     tileProviders: TileProviders({'protomaps': prov}),
