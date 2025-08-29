428cf87911d94ccf6a06e563aa1b1701be2ae6c2
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-08-29 03:31:19 +0200

    build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,40 +1,6 @@
-// android/app/build.gradle.kts
-
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("dev.flutter.flutter-gradle-plugin")   // nincs version!
-}
-
-android {
-    namespace = "com.example.ai_navigation_flutter"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
-    defaultConfig {
-        applicationId = "com.example.ai_navigation_flutter"
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-        isCoreLibraryDesugaringEnabled = true
-    }
-
-    kotlinOptions { jvmTarget = JavaVersion.VERSION_17.toString() }
-
-    buildTypes {
-        release {
-            // maradhat debug aláírás, ha CI-hez kell csak
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
-}
-
-dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    id("dev.flutter.flutter-plugin-loader")
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    id("org.jetbrains.kotlin.android") apply false
 }
