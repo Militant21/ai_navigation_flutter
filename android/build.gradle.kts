e70aa33139f3f2a582abd36f7224b2797c202f07
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-08-29 02:24:32 +0200

    build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application") apply false
     id("com.android.library") apply false
     id("org.jetbrains.kotlin.android") apply false
+    id("dev.flutter.flutter-gradle-plugin") apply false
 }
 
 allprojects {
