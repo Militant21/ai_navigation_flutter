a068f98b3558e25f9cc19cc7fdc86d6a7d108878
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-08-29 02:28:56 +0200

    build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,21 +1,17 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
-    // A Flutter Gradle Plugin az Android és Kotlin plugin után legyen
+    id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.example.ai_navigation_flutter"
-
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        // Stabil beállítás AGP-hez
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-        // <-- kell a flutter_local_notifications miatt
         isCoreLibraryDesugaringEnabled = true
     }
 
@@ -24,29 +20,24 @@ android {
     }
 
     defaultConfig {
-        // Állítsd a saját csomagnevedre, ha kell
         applicationId = "com.example.ai_navigation_flutter"
-
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // Példa: debug kulccsal írjuk alá, hogy a CI tudjon release-t építeni
             signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
 
 dependencies {
-    // Desugaring JDK könyvtárakhoz (Java 8+ API-k régebbi Androidra)
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
 
 flutter {
-    source = "../.."
+    source = "../../.."
 }
