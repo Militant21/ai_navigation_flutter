a1d734572ca3c9abaf32349beb935ca1e53b581b
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-08-29 02:14:54 +0200

    build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application") version "8.9.1" apply false
-    id("com.android.library") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    id("org.jetbrains.kotlin.android") apply false
 }
 
 allprojects {
