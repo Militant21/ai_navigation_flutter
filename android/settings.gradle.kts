82b0908cecdf77f5b1f524879168d13f95557423
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-08-29 14:49:23 +0200

    settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -6,20 +6,8 @@ pluginManagement {
     }
 }
 
-val props = java.util.Properties()
-val lp = file("local.properties")
-if (lp.exists()) {
-    lp.inputStream().use { props.load(it) }
-}
-val flutterSdk: String? = props.getProperty("flutter.sdk") ?: System.getenv("FLUTTER_ROOT")
-if (flutterSdk != null) {
-    includeBuild("$flutterSdk/packages/flutter_tools/gradle")
-} else {
-    logger.warn("⚠ 'flutter.sdk' nincs beállítva (android/local.properties vagy FLUTTER_ROOT).")
-}
-
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
         google()
         mavenCentral()
