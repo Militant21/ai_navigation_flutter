5ba230ee114dfb438daed00149edb924dbd163b0
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-08-29 05:31:38 +0200

    settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,3 +1,5 @@
+// android/settings.gradle.kts
+
 import java.io.FileInputStream
 import java.util.Properties
 
@@ -7,27 +9,28 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-
-    // Flutter SDK beemelése a lokalból
-    val props = Properties()
-    val lp = file("local.properties")
-    if (lp.exists()) {
-        FileInputStream(lp).use { props.load(it) }
-    }
-    val flutterSdk: String? = props.getProperty("flutter.sdk")
-    if (flutterSdk != null) {
-        includeBuild("$flutterSdk/packages/flutter_tools/gradle")
-    } else {
-        logger.warn("⚠ 'flutter.sdk' nincs beállítva az android/local.properties-ben.")
+    // A pluginek verziói ITT legyenek rögzítve
+    plugins {
+        id("com.android.application") version "8.1.4"
+        id("com.android.library") version "8.1.4"
+        id("org.jetbrains.kotlin.android") version "1.9.24"
+        id("dev.flutter.flutter-gradle-plugin") version "1.0.0"
     }
 }
 
-// Itt KELL rögzíteni a plugin verziókat
-plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.1.4" apply false
-    id("com.android.library") version "8.1.4" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+// Flutter SDK helye a local.properties-ből
+val props = Properties()
+val lp = file("local.properties")
+if (lp.exists()) {
+    FileInputStream(lp).use { props.load(it) }
+}
+val flutterSdk: String? = props.getProperty("flutter.sdk")
+
+// A Flutter gradle plugin include-buildje a Flutter SDK-ból
+if (flutterSdk != null) {
+    includeBuild("$flutterSdk/packages/flutter_tools/gradle")
+} else {
+    logger.warn("⚠ 'flutter.sdk' nincs beállítva az android/local.properties-ben.")
 }
 
 dependencyResolutionManagement {
