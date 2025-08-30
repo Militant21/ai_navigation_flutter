1a9138f6da0060fb2a5ef07a167533f314385725
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-08-30 18:55:35 +0200

    settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,1 +1,15 @@
 
+pluginManagement {
+    includeBuild("app/flutter/ephemeral/.android/include_flutter.groovy")
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ai_navigation_flutter"
+include(":app")
