79c617a2aed926374781a2984a6c68678580ad42
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-08-30 19:21:09 +0200

    settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,15 +1,31 @@
 pluginManagement {
-    // A JAVÍTÁS: A helyes relatív útvonal a projekt gyökerében lévő generált fájlhoz
-    includeBuild("../ephemeral/.android/include_flutter.groovy")
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
+        // Ez a sor mondja meg, HOL van a Flutter plugin
+        maven {
+            url = uri("https://storage.googleapis.com/download.flutter.io")
+        }
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        // Ez a sor a többi Flutter csomaghoz kell
+        maven {
+            url = uri("https://storage.googleapis.com/download.flutter.io")
+        }
     }
 }
 
 rootProject.name = "ai_navigation_flutter"
 include(":app")
+
+// Ez a sor mondja meg, HOGY a Flutter plugin létezik
+plugins {
+    id("dev.flutter.flutter-gradle-plugin").version("1.0.0").apply(false)
+}
