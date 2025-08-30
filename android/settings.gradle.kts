b0e608ed1770f88757b924ea171b1a60ab732dce
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-08-30 22:22:13 +0200

    settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,24 +1,9 @@
-import java.io.File
-import java.util.Properties
-
-// Beolvassuk a "local.properties" fájlt, hogy megtudjuk, hol van a Flutter SDK
-val localPropertiesFile = File(rootDir, "local.properties")
-val properties = Properties()
-if (localPropertiesFile.exists()) {
-    properties.load(java.io.FileInputStream(localPropertiesFile))
+pluginManagement {
+    includeBuild("app/flutter/ephemeral/.android/include_flutter.groovy")
 }
 
-val flutterSdkPath = properties.getProperty("flutter.sdk")
-if (flutterSdkPath == null) {
-    throw GradleException("Flutter SDK not found. Define location in local.properties.")
-}
-
-// A régi, direkt módszer, ami a HELYI fájlt használja, nem a távoli tárolót
-apply(from = "$flutterSdkPath/packages/flutter_tools/gradle/flutter.gradle")
-
-// A szokásos projektbeállítások
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
