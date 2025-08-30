6470fd65e42019f6e2b17582ab6c5e4443909388
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-08-30 07:48:45 +0200

    build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,18 +1,5 @@
 import org.gradle.api.file.Directory
 
-// --- Repositories minden modulnak ---
-// (Ha a settings-ben PREFER_PROJECT van, ezek lesznek érvényben;
-// ha PREFER_SETTINGS, akkor sem árt, de nem zavar be.)
-subprojects {
-    repositories {
-        google()
-        mavenCentral()
-        // Flutter engine / artifacts
-        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
-    }
-}
-
-// --- Build mappa központosítása (ahogy nálad is volt) ---
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.set(newBuildDir)
 
