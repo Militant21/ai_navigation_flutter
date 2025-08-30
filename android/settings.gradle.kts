26bb56a51a3ba8cc225ee12863724464fbea74b7
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-08-30 17:25:34 +0200

    settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -16,3 +16,10 @@ dependencyResolutionManagement {
 
 rootProject.name = "ai_navigation_flutter"
 include(":app")
+
+// --- HIÁNYZÓ RÉSZ ---
+// Ez a blokk megmondja a buildnek, hogy a Flutter plugin létezik
+// és elérhető a projekt számára.
+plugins {
+    id("dev.flutter.flutter-gradle-plugin").version("1.0.0").apply(false)
+}
