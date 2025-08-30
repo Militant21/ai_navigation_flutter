994ba3b6aeb5f05b6a68a2f79ea3b4786fa9fa6f
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-08-30 22:23:05 +0200

    build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,1 +1,5 @@
-// This file is intentionally left blank.
+plugins {
+    id("com.android.application").version("8.2.0").apply(false)
+    id("org.jetbrains.kotlin.android").version("1.9.10").apply(false)
+    id("dev.flutter.flutter-gradle-plugin").version("1.0.0").apply(false)
+}
