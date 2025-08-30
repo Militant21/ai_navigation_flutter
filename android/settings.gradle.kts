4b107e0b901b5b8c1c8bb820ba988d9e8e068540
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-08-30 07:50:04 +0200

    settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -3,11 +3,7 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-    }
-    plugins {
-        id("com.android.application") version "8.6.0"
-        id("org.jetbrains.kotlin.android") version "2.1.0"
-        id("dev.flutter.flutter-gradle-plugin")
+        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
     }
 }
 
@@ -16,21 +12,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
     }
-}
-
-// Flutter SDK bekötése (a Flutter action már írja a local.properties-t)
-val props = java.util.Properties()
-val lp = file("local.properties")
-if (lp.exists()) {
-    lp.inputStream().use { props.load(it) }
-}
-val flutterSdk: String? = props.getProperty("flutter.sdk") ?: System.getenv("FLUTTER_ROOT")
-
-if (flutterSdk != null) {
-    includeBuild("$flutterSdk/packages/flutter_tools/gradle")
-} else {
-    logger.warn("⚠️ A 'flutter.sdk' nincs beállítva (android/local.properties vagy env).")
 }
 
 rootProject.name = "ai_navigation_flutter"
