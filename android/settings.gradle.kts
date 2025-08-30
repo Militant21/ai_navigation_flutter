b801f663b81165326ffca02ce5c605eb1aef0cc2
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-08-30 17:20:57 +0200

    settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -5,22 +5,14 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
-        // Flutter binárisok (embedding) repoja
-        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
     }
 }
+
 rootProject.name = "ai_navigation_flutter"
 include(":app")
-
-// EZ KELL: a Flutter loader plugin
-plugins {
-    id("dev.flutter.flutter-plugin-loader")
-}
-
-// Ha nálad van FLUTTER_ROOT vagy local.properties-ben flutter.sdk, nem kell semmi plusz.
-// (Régi: apply-from / includeBuild hackeket hagyd ki.)
