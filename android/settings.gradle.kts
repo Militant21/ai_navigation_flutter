b4018608bd6af2151cd987113b30e3473810cca9
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-08-30 22:10:15 +0200

    settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,31 +1,29 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-        // Ez a sor mondja meg, HOL van a Flutter plugin
-        maven {
-            url = uri("https://storage.googleapis.com/download.flutter.io")
-        }
-    }
+import java.io.File
+import java.util.Properties
+
+// Beolvassuk a "local.properties" fájlt, hogy megtudjuk, hol van a Flutter SDK
+val localPropertiesFile = File(rootDir, "local.properties")
+val properties = Properties()
+if (localPropertiesFile.exists()) {
+    properties.load(java.io.FileInputStream(localPropertiesFile))
 }
 
+val flutterSdkPath = properties.getProperty("flutter.sdk")
+if (flutterSdkPath == null) {
+    throw GradleException("Flutter SDK not found. Define location in local.properties.")
+}
+
+// A régi, direkt módszer, ami a HELYI fájlt használja, nem a távoli tárolót
+apply(from = "$flutterSdkPath/packages/flutter_tools/gradle/flutter.gradle")
+
+// A szokásos projektbeállítások
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
-        // Ez a sor a többi Flutter csomaghoz (engine, stb.) kell
-        maven {
-            url = uri("https://storage.googleapis.com/download.flutter.io")
-        }
     }
 }
 
 rootProject.name = "ai_navigation_flutter"
 include(":app")
-
-// Ez a blokk "regisztrálja" a Flutter plugint, hogy a többi fájl használni tudja
-plugins {
-    id("dev.flutter.flutter-gradle-plugin").version("1.0.0").apply(false)
-}
