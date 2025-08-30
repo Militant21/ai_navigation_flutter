f150eb5b40ab70195292aade1b780ee956925701
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-08-30 18:59:32 +0200

    build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,16 +1,5 @@
-import org.gradle.api.file.Directory
-
-// Ne adjunk meg itt repositories-t, mert a settings-ben központosítjuk!
-
-// build/ mappa központosítása (ha kell)
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.set(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.set(newSubprojectBuildDir)
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+plugins {
+    id("com.android.application").version("8.2.0").apply(false)
+    id("org.jetbrains.kotlin.android").version("1.9.10").apply(false)
+    id("dev.flutter.flutter-gradle-plugin").version("1.0.0").apply(false)
 }
