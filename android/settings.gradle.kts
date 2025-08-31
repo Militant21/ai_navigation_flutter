752f4409ad3166c97af1a392ca35c74613ae384a
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-08-31 08:45:26 +0200

    settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,50 +1,14 @@
-// android/settings.gradle.kts
-import java.io.File
-import java.util.Properties
-
 pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
-    }
-}
-
-plugins {
-    // NINCS verzió megadva!
-    id("dev.flutter.flutter-plugin-loader")
+    includeBuild("../ephemeral/.android/include_flutter.groovy")
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
     }
 }
 
 rootProject.name = "ai_navigation_flutter"
 include(":app")
-
-// Flutter SDK útvonal felderítése
-fun flutterSdkFromLocalProperties(): String? {
-    val f = File("local.properties")
-    if (!f.exists()) return null
-    val p = Properties()
-    f.inputStream().use { p.load(it) }
-    return p.getProperty("flutter.sdk")
-}
-
-val flutterSdkPath: String? =
-    System.getenv("FLUTTER_ROOT")
-        ?: System.getenv("FLUTTER_HOME")
-        ?: flutterSdkFromLocalProperties()
-
-if (flutterSdkPath != null) {
-    println("Including Flutter gradle from: $flutterSdkPath")
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
-} else {
-    println("WARNING: Flutter SDK path not found.")
-}
