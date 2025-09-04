a70f1b1ea431082a2e69c983a8a96b9102b003f3
Militant21 <35631727+Militant21@users.noreply.github.com>
2025-09-04 17:41:02 +0200

    Create location _controller.dart

---
+++ lib/controllers/location _controller.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class LocationController {
+  LocationController._();
+  static final instance = LocationController._();
+
+  final ValueNotifier<Position?> position = ValueNotifier<Position?>(null);
+  bool followMe = true;
+  StreamSubscription<Position>? _sub;
+
+  Future<void> start() async {
+    if (!await Geolocator.isLocationServiceEnabled()) return;
+    var perm = await Geolocator.checkPermission();
+    if (perm == LocationPermission.denied) perm = await Geolocator.requestPermission();
+    if (perm == LocationPermission.denied || perm == LocationPermission.deniedForever) return;
+
+    position.value = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+    _sub?.cancel();
+    _sub = Geolocator.getPositionStream().listen((p) => position.value = p);
+  }
+
+  void stop() => _sub?.cancel();
+
+  void centerOnMap(MapController mapCtrl) {
+    final p = position.value;
+    if (p == null) return;
+    mapCtrl.move(LatLng(p.latitude, p.longitude), mapCtrl.camera.zoom);
+  }
+}
